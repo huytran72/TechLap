31043997cf14ba618095d4bf271212e09279c97d
Huy Tran <baohuy82.th@gmail.com>
2025-09-06 00:45:14 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const express = require("express")
 const app = express()
 const PORT = 8080
 
-app.get("/", () => {
-  return "Hello World"
+app.get("/", (req, res) => {
+  res.send("Hello World")
 })
 
 app.listen(PORT, () => {
