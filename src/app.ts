6aea7434baf8951ea55bf556a64df6bdc4ac3878
Huy Tran <baohuy82.th@gmail.com>
2025-09-06 09:50:10 -0700

    Create app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+// const express = require("express")
+
+import express from "express"
+
+const app = express()
+const PORT = 8080
+
+app.get("/", (req, res) => {
+  res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
