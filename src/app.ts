3e2eeb02303440aba729517a6b5f2a94b3c53054
Huy Tran <baohuy82.th@gmail.com>
2025-09-07 13:39:20 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,10 @@ app.get("/", (req, res) => {
   res.send("Hello World!!!")
 })
 
+app.get("/about", (req, res) => {
+  res.send("About Us")
+})
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
