65550b511e72cde6918bfe0988eed6a8dededae7
Huy Tran <baohuy82.th@gmail.com>
2025-09-07 14:21:53 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import express from "express"
 import "dotenv/config"
 
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
   res.send("Hello World nodemon")
