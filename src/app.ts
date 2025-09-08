ec78f7bb46ae3f9231e824848f5c3ba0deff1d55
Huy Tran <baohuy82.th@gmail.com>
2025-09-08 09:29:20 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,18 +10,6 @@ const PORT = process.env.PORT || 8080
 app.set("view engine", "ejs")
 app.set("views", "src/views")
 
-app.get("/", (req, res) => {
-  res.render("home.ejs")
-})
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About Us</h1>")
-})
-
-app.get("/contact", (req, res) => {
-  res.send("Contact Us ............")
-})
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
   console.log("ENV PORT:", process.env.PORT)
