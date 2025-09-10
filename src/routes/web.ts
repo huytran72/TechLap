5826dacedacc5faa2ba23606341db620c403d589
Huy Tran <baohuy82.th@gmail.com>
2025-09-09 23:02:38 -0700

    Update web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -2,15 +2,16 @@ import express from "express"
 
 const router = express.Router()
 
-const webRoutes = () => {}
-router.get("/", (req, res) => {
-  res.render("home.ejs")
-})
+const webRoutes = (app) => {
+  router.get("/", (req, res) => {
+    res.render("home.ejs")
+  })
 
-router.get("/about", (req, res) => {
-  res.send("<h1>About Us</h1>")
-})
+  router.get("/about", (req, res) => {
+    res.send("<h1>About Us</h1>")
+  })
 
-router.get("/contact", (req, res) => {
-  res.send("Contact Us ............")
-})
+  router.get("/contact", (req, res) => {
+    res.send("Contact Us ............")
+  })
+}
