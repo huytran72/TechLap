36f9cfc27c77a5e49c2c425f4bda8b83abd85107
Huy Tran <baohuy82.th@gmail.com>
2025-09-09 23:04:06 -0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@
 
 import express from "express"
 import "dotenv/config"
+import webRoutes from "./routes/web"
 
 const app = express()
 const PORT = process.env.PORT || 8080
@@ -14,3 +15,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
   console.log("ENV PORT:", process.env.PORT)
 })
+
+webRoutes(app)
