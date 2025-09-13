e1c1401ffd4c0f464aebf8a19ba404b1ea741cb7
Huy Tran <baohuy82.th@gmail.com>
2025-09-13 09:46:05 -0700

    Update web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,19 +1,12 @@
 import express, { Express } from "express"
 import { get } from "http"
-import { getHomePage } from "../controllers/user.controller"
+import { getCreateUserPage, getHomePage } from "../controllers/user.controller"
 
 const router = express.Router()
 
 const webRoutes = (app: Express) => {
   router.get("/", getHomePage)
-
-  router.get("/create-user", (req, res) => {
-    res.send("<h1>About Us......</h1>")
-  })
-
-  router.get("/contact", (req, res) => {
-    res.send("Contact Us ............")
-  })
+  router.get("/create-user", getCreateUserPage)
 
   app.use("/", router)
 }
