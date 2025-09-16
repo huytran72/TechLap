f3b211e6e59a852efd14f0e37f250b6661697042
Huy Tran <baohuy82.th@gmail.com>
2025-09-15 23:35:30 -0700

    Update web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -7,7 +7,7 @@ const router = express.Router()
 const webRoutes = (app: Express) => {
   router.get("/", getHomePage)
   router.get("/create-user", getCreateUserPage)
-  router.post("/handle-create-user", getCreateUserPage)
+  router.post("/handle-create-user", postCreateUser)
 
   app.use("/", router)
 }
