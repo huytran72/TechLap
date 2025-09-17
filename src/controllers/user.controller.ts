cfd8acc6beb65b588e3abd47aa5c310ff5d41ebe
Huy Tran <baohuy82.th@gmail.com>
2025-09-16 23:49:42 -0700

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,6 +9,7 @@ const getCreateUserPage = (req: Request, res: Response) => {
 }
 
 const postCreateUser = (req: Request, res: Response) => {
+  console.log("check request", req.body)
   return res.redirect("/")
 }
 
