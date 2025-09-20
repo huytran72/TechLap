5b7a5f80b88ba001395e5d95bf4fc2b8968f5dc2
Huy Tran <baohuy82.th@gmail.com>
2025-09-20 15:14:00 -0700

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,8 +2,10 @@
 import mysql from "mysql2/promise"
 
 // Create the connection to database
-const connection = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  database: "test",
-})
+const getConnection = async () => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "test",
+  })
+}
