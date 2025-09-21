ac07784ee9070c2c666fafda6933e3229259bd48
Huy Tran <baohuy82.th@gmail.com>
2025-09-20 22:47:13 -0700

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -14,14 +14,12 @@ const getConnection = async () => {
 
 // A simple SELECT query
 try {
-  const [results, fields] = await connection.query(
-    'SELECT * FROM `users`
-  );
+  const [results, fields] = await connection.query("SELECT * FROM `users`")
 
-  console.log(results); // results contains rows returned by server
-  console.log(fields); // fields contains extra meta data about results, if available
+  console.log(results) // results contains rows returned by server
+  console.log(fields) // fields contains extra meta data about results, if available
 } catch (err) {
-  console.log(err);
+  console.log(err)
 }
 
 export { getConnection }
