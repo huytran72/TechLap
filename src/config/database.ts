c740169aab13961d3bae8fa37093cf8b6868a764
Huy Tran <baohuy82.th@gmail.com>
2025-09-20 22:51:11 -0700

    Update database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,16 +10,16 @@ const getConnection = async () => {
     password: "123456",
     database: "nodejspro",
   })
-}
 
-// A simple SELECT query
-try {
-  const [results, fields] = await connection.query("SELECT * FROM `users`")
+  // A simple SELECT query
+  try {
+    const [results, fields] = await connection.query("SELECT * FROM `users`")
 
-  console.log(results) // results contains rows returned by server
-  console.log(fields) // fields contains extra meta data about results, if available
-} catch (err) {
-  console.log(err)
+    console.log(results) // results contains rows returned by server
+    console.log(fields) // fields contains extra meta data about results, if available
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export { getConnection }
