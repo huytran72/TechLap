d94b2b619464756e5a4c656a547561c9ba5c3baf
Huy Tran <baohuy82.th@gmail.com>
2025-09-21 23:18:51 -0700

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,12 @@
+import { get } from "http"
+import { getConnection } from "../config/database"
+
 const handleCreateUser = (fullName: string, email: string, address: string) => {
   console.log(">>> Insert new user to DB:", { fullName, email, address })
 }
 
-const getAllUsers = () => {
+const getAllUsers = async () => {
+  const connection = await getConnection()
   return "List of users from DB"
 }
 
