90afee84dee85ec7259c8be39ef92a006ab9c93f
Huy Tran <baohuy82.th@gmail.com>
2025-09-30 23:27:41 -0700

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -48,10 +48,10 @@ const handleDeleteUser = async (id: string) => {
   }
 }
 
-const handleDeleteUser = async (id: string) => {
+const getUserById = async (id: string) => {
   try {
     const connection = await getConnection()
-    const sql = "DELETE FROM `users` WHERE `id` = ?"
+    const sql = "SELECT * FROM `users` WHERE `id` = ?"
     const values = [id]
 
     const [result, fields] = await connection.execute(sql, values)
