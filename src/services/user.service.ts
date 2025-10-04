3bb96b88f6f776418b43c2f43df17c207af6000a
Huy Tran <baohuy82.th@gmail.com>
2025-10-04 00:09:14 -0700

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -54,7 +54,7 @@ const getUserById = async (id: string) => {
     const sql = "SELECT * FROM `users` WHERE `id` = ?"
     const values = [id]
     const [result, fields] = await connection.execute(sql, values)
-    return result
+    return result[0]
   } catch (err) {
     console.log(err)
     return []
