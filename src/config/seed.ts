db87d4f98936c7e1e2293ce5fc4efba3682335c9
Huy Tran <baohuy82.th@gmail.com>
2025-10-16 23:15:51 -0700

    Update seed.ts

--- src/config/seed.ts
+++ src/config/seed.ts
@@ -1,2 +1,17 @@
-const innitDatabase = async () => {}
+const innitDatabase = async () => {
+  await prisma.user.createMany({
+    data: [
+      {
+        username: "user1",
+        password: "password1",
+        accountType: "user",
+      },
+      {
+        username: "admin",
+        password: "adminpassword",
+        accountType: "admin",
+      },
+    ],
+  })
+}
 export default innitDatabase
