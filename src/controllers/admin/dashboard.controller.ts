9701a8869f75729bdeb91c9c9b51f8d2e0cade76
Huy Tran <baohuy82.th@gmail.com>
2025-11-02 22:36:15 -0800

    Update dashboard.controller.ts

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express"
+import { getAllUsers } from "services/user.service"
 const getDashboardPage = async (req: Request, res: Response) => {
   return res.render("admin/dashboard/show.ejs")
 }
 
 const getAdminUserPage = async (req: Request, res: Response) => {
   const users = await getAllUsers()
-  return res.render("admin/user/show.ejs")
+  return res.render("admin/user/show.ejs", { users })
 }
 
 const getAdminProductPage = async (req: Request, res: Response) => {
