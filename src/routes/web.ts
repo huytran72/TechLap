ea2fffd7b2c3b3bdcce0ee61705a9fcb44db84fb
Huy Tran <baohuy82.th@gmail.com>
2025-11-07 23:11:50 -0800

    Update web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -18,7 +18,6 @@ const router = express.Router()
 
 const webRoutes = (app: Express) => {
   router.get("/", getHomePage)
-  router.get("/create-user", getCreateUserPage)
   router.post("/handle-create-user", postCreateUser)
   router.post("/handle-delete-user/:id", postDeleteUser)
   router.get("/handle-view-user/:id", getViewUser)
@@ -27,6 +26,8 @@ const webRoutes = (app: Express) => {
   //admin routes
   router.get("/admin", getDashboardPage)
   router.get("/admin/user", getAdminUserPage)
+  router.get("/admin/create-user", getCreateUserPage)
+
   app.use("/", router)
 }
 
