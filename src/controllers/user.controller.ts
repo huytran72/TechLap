5d926ec45ef40abea3fe4d73c1a01d0b6a7893e9
Huy Tran <baohuy82.th@gmail.com>
2025-11-18 22:54:51 -0800

    Update user.controller.ts

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -19,7 +19,7 @@ const getCreateUserPage = async (req: Request, res: Response) => {
 }
 
 const postCreateUser = async (req: Request, res: Response) => {
-  const { name, username, phone, address } = req.body
+  const { name, username, phone, role, address } = req.body
 
   //handle data here
   // await handleCreateUser(name, email, address)
