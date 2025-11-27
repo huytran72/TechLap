1280799e09afb4f4b0178142a4f23a9fb7fc6d12
Huy Tran <baohuy82.th@gmail.com>
2025-11-27 00:01:35 -0800

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -57,8 +57,8 @@ const updateUserById = async (
       name: fullName,
       email: email,
       address: address,
-      password: "",
-      accountType: "",
+      // password: "",
+      // accountType: "",
     },
   })
   return updatedUser
