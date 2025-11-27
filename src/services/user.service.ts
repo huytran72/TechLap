7001f272d887f3d649aa228a30da38ca11db5462
Huy Tran <baohuy82.th@gmail.com>
2025-11-26 22:55:17 -0800

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,7 +2,7 @@ import { prisma } from "config/client"
 import { ACCOUNT_TYPE } from "config/constant"
 
 const handleCreateUser = async (
-  name: string,
+  fullName: string,
   email: string,
   address: string,
   phone: string,
@@ -10,7 +10,7 @@ const handleCreateUser = async (
 ) => {
   const newUser = await prisma.user.create({
     data: {
-      name: name,
+      fullName: fullName,
       username: email,
       address: address,
       password: "123456",
