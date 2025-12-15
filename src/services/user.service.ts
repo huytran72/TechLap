0ac7ba8bb6c6015d2d84494bb98a9ce7f59899ec
Huy Tran <baohuy82.th@gmail.com>
2025-12-14 22:38:15 -0800

    Update user.service.ts

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,6 +3,11 @@ import { ACCOUNT_TYPE } from "config/constant"
 import bcrypt from "bcrypt"
 const saltRounds = 10
 
+const hashPassword = async (password: string) => {
+  const hashedPassword = await bcrypt.hash(password, saltRounds)
+  return hashedPassword
+}
+
 const handleCreateUser = async (
   fullName: string,
   email: string,
