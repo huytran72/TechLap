75686a4e9e330bb51c7febb1afcac09b73a933fa
Huy Tran <baohuy82.th@gmail.com>
2025-12-23 23:34:27 -0800

    Update web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -31,7 +31,7 @@ const webRoutes = (app: Express) => {
     fileUploadMiddleware("avatar"),
     postCreateUser
   )
-  router.post("/handle-delete-user/:id", postDeleteUser)
+  router.post("/admin/delete-user/:id", postDeleteUser)
   router.get("/admin/product", getAdminProductPage)
   router.get("/admin/order", getAdminOrderPage)
 
