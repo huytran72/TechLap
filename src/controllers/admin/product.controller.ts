fd372c8573497bd9227af0d33466ed52d090dd5f
Huy Tran <baohuy82.th@gmail.com>
2026-01-18 13:44:54 -0800

    Update product.controller.ts

--- src/controllers/admin/product.controller.ts
+++ src/controllers/admin/product.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express"
 
 const getAdminCreateProductPage = async (req: Request, res: Response) => {
-  return res.render("client/product/detail.ejs")
+  return res.render("admin/product/create.ejs")
 }
 
 export { getAdminCreateProductPage }
