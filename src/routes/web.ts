3368d3980ffa9c4554265594cb7d4f97dfc36762
Huy Tran <baohuy82.th@gmail.com>
2026-01-23 20:28:44 -0800

    Update web.ts

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -32,21 +32,26 @@ const webRoutes = (app: Express) => {
   router.post(
     "/admin/handle-create-user",
     fileUploadMiddleware("avatar"),
-    postCreateUser
+    postCreateUser,
   )
   router.post("/admin/delete-user/:id", postDeleteUser)
   router.get("/admin/view-user/:id", getViewUser)
   router.post(
     "/admin/update-user",
     fileUploadMiddleware("avatar"),
-    postUpdateUser
+    postUpdateUser,
   )
 
   router.get("/admin/product", getAdminProductPage)
   router.get(
     "/admin/create-product",
     fileUploadMiddleware("image", "images/product"),
-    getAdminCreateProductPage
+    getAdminCreateProductPage,
+  )
+  router.post(
+    "/admin/create-product",
+    fileUploadMiddleware("image", "images/product"),
+    postAdminCreateProductPage,
   )
 
   router.get("/admin/order", getAdminOrderPage)
