bba5281ade1f32d03e6e37fca6e181182ec0da2b
Huy Tran <baohuy82.th@gmail.com>
2026-01-27 22:42:51 -0800

    Update product.schema.ts

--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -1,15 +1,15 @@
 import { z } from "zod"
 
 export const ProductSchema = z.object({
-  username: z.string(),
+  name: z.string().trim().min(1),
 
-   name	      String @db.VarChar(255)
-  price		    Int 
-  image		    String @db.VarChar(255)
-  detailDesc	String @db.VarChar(255)
-  shortDesc	  String @db.VarChar(255)
-  quantity	  Int
-  sold		    String @db.VarChar(255)
-  factory	    String @db.VarChar(255)
-  target		  String @db.VarChar(255)
+  //  name	      String @db.VarChar(255)
+  // price		    Int
+  // image		    String @db.VarChar(255)
+  // detailDesc	String @db.VarChar(255)
+  // shortDesc	  String @db.VarChar(255)
+  // quantity	  Int
+  // sold		    String @db.VarChar(255)
+  // factory	    String @db.VarChar(255)
+  // target		  String @db.VarChar(255)
 })
