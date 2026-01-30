c829f9257394b761edc52a11abbcc0827de85679
Huy Tran <baohuy82.th@gmail.com>
2026-01-29 23:05:21 -0800

    Update product.schema.ts

--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -8,14 +8,6 @@ export const ProductSchema = z.object({
   quantity: z.number().min(0),
   factory: z.string().trim().min(1),
   target: z.string().trim().min(1),
-
-  //  name	      String @db.VarChar(255)
-  // price		    Int
-  // image		    String @db.VarChar(255)
-  // detailDesc	String @db.VarChar(255)
-  // shortDesc	  String @db.VarChar(255)
-  // quantity	  Int
-  // sold		    String @db.VarChar(255)
-  // factory	    String @db.VarChar(255)
-  // target		  String @db.VarChar(255)
 })
+
+export type TProductSchema = z.infer<typeof ProductSchema>
