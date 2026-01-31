cddfde9919bc458733657350e5f752d65df21dfd
Huy Tran <baohuy82.th@gmail.com>
2026-01-31 13:56:19 -0800

    Update product.schema.ts

--- src/validation/product.schema.ts
+++ src/validation/product.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 
 export const ProductSchema = z.object({
   name: z.string().trim().min(1, { message: "Name is required" }),
-  price: z.number().min(0),
+  price: z.number().min(0, { message: "Price must be non-negative" }),
   detailDesc: z.string().trim().min(1),
   shortDesc: z.string().trim().min(1).optional(),
   quantity: z.number().min(0),
